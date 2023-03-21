0b91c7c91d2a4f3957adfe7f2b953e86c64e8066
yashjariwala01 <122197521+yashjariwala01@users.noreply.github.com>
2023-03-21 13:59:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function fun(){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve("Hello, world!")
+		},1000)
+})
+}
+fun().then(data=>{
+document.getElementById("output").innerText= data;
+})
